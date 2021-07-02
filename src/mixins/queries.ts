b1401725400500c25ea591d723de086a71934873
Joss Doebler <mail@joss-doebler.de>
2021-07-02 18:13:45 +0200

    feat: add mixins for media queries

---
+++ src/mixins/queries.ts
@@ -0,0 +1,35 @@
+import { css } from 'styled-components';
+import type { FlattenSimpleInterpolation } from 'styled-components';
+import { breakpoints } from '../variables/breakpoints';
+
+export function untilMobile(styles: FlattenSimpleInterpolation) {
+  return css`
+    @media (max-width: ${breakpoints.mobile.max}px) {
+      ${styles}
+    }
+  `;
+}
+
+export function fromTablet(styles: FlattenSimpleInterpolation) {
+  return css`
+    @media (min-width: ${breakpoints.tablet.min}px) {
+      ${styles}
+    }
+  `;
+}
+
+export function untilTablet(styles: FlattenSimpleInterpolation) {
+  return css`
+    @media (max-width: ${breakpoints.tablet.max}px) {
+      ${styles}
+    }
+  `;
+}
+
+export function fromDesktop(styles: FlattenSimpleInterpolation) {
+  return css`
+    @media (min-width: ${breakpoints.desktop.min}px) {
+      ${styles}
+    }
+  `;
+}
