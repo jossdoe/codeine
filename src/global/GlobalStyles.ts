432897ae6a384426bc5e14dd9a98a90bf58beb5f
Joss Doebler <mail@joss-doebler.de>
2021-07-03 12:29:32 +0200

    feat: add global styles with styled-reset

---
+++ src/global/GlobalStyles.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import { gray } from '@/variables/colors';
+
+export const GlobalStyles = createGlobalStyle`
+  ${reset}
+
+  @import url('https://fonts.googleapis.com/css?family=Inter:400');
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    color: ${gray(700)};
+  }
+
+  a, a:visited {
+    color: inherit;
+    text-decoration: inherit;
+  }
+`;
